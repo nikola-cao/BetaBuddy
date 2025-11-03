0380c6ff54126b52af3f95cc339219729a8aacd3
nikola-cao <cao.nikola.a@gmail.com>
2025-11-03 15:37:49 -0500

    initial

---
+++ BetaBuddy/BetaBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetaBuddyApp.swift
+//  BetaBuddy
+//
+//  Created by Nikola Cao on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct BetaBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
